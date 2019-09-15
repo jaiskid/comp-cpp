7b5355be203836c8cca9e99c5abd967b8000e2b9
Jaiskid <neeraj001jaiswal@gmail.com>
2019-09-15 22:23:16 +0530

    codeupdate

--- issorted.cpp
+++ issorted.cpp
@@ -4,8 +4,10 @@ bool isSorted(int *arr,int n){
 	if(n==1){
 	 return true;
 	}
-	if(arr[0]<=arr[1]&&isSorted(arr+1,n-1));
+	if(arr[0]<=arr[1]&&isSorted(arr+1,n-1))
+	{
 	return true;
+	}
 	return false;
 }
 int main(){
